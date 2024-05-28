5fe4ad4d9b0b1ca8594da46e7465380b0da92aa8
miki <100952421+mikieoo@users.noreply.github.com>
2024-05-28 21:15:27 +0900

    [level 0] Title: 약수 구하기, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList solution(int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                list.add(i);
+            }
+        }
+        
+        return list;
+    }
+}
