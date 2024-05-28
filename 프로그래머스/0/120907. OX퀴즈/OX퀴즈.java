8ab9e10d4ab2cec28e2a372049427424a731f2ed
miki <100952421+mikieoo@users.noreply.github.com>
2024-05-28 21:54:20 +0900

    [level 0] Title: OX퀴즈, Time: 0.09 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
@@ -1,34 +1,20 @@
-import java.util.*;
-
 class Solution {
     public String[] solution(String[] quiz) {
-        List<String> list = new ArrayList<>();
+        String[] answer = new String[quiz.length];
 
-        for (String q : quiz) {
-            String[] parts = q.split(" ");
-            int op1 = Integer.parseInt(parts[0]);
-            String operator = parts[1];
-            int op2 = Integer.parseInt(parts[2]);
-            int result = Integer.parseInt(parts[4]);
-
-            int cal_result = 0;
-            switch (operator) {
+        for(int i=0; i<quiz.length; i++){
+            String[] arr = quiz[i].split(" ");
+            int num1 = Integer.parseInt(arr[0]);
+            int num2 = Integer.parseInt(arr[2]);
+            int result = Integer.parseInt(arr[4]);
+            switch (arr[1]){
                 case "+":
-                    cal_result = op1 + op2; break;
+                    answer[i] = num1 + num2 == result ? "O" : "X";
+                    break;
                 case "-":
-                    cal_result = op1 - op2; break;
+                    answer[i] = num1 - num2 == result ? "O" : "X";
+                    break;
             }
-
-            if (cal_result == result) {
-                list.add("O");
-            } else {
-                list.add("X");
-            }
-        }
-
-        String[] answer = new String[list.size()];
-        for (int i=0; i<answer.length; i++) {
-            answer[i] = list.get(i);
         }
         
         return answer;
