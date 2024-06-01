82a98ad01ea93e9c674b217e1a51c6ead49cdce4
miki <100952421+mikieoo@users.noreply.github.com>
2024-06-02 01:43:30 +0900

    [level 0] Title: 문자열 밀기, Time: 0.03 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,15 +1,5 @@
 class Solution {
     public int solution(String A, String B) {
-        int cnt = 0;
-        for(int i=0; i<A.length()+1; i++){
-            if(A.equals(B)){
-                return cnt;
-            }
-            B = B.substring(1) + B.charAt(0);
-            cnt++;
-        }
-
-        if(cnt > A.length()) return -1;
-        else return cnt;
+        return B.repeat(2).indexOf(A);
     }
 }
