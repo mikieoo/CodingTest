7bba878218772d37151277f3bf8bd5afbc357c9a
miki <100952421+mikieoo@users.noreply.github.com>
2026-01-20 17:08:29 +0900

    [level 2] Title: 올바른 괄호, Time: 17.66 ms, Memory: 55.2 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,26 +1,22 @@
-import java.util.Stack;
+import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        boolean answer = false;
 
         Stack<Character> stack = new Stack<>();
 
         for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c == '(') {
+            if (s.charAt(i) == '(') {
                 stack.push(s.charAt(i));
-            } else if (c == ')'){
-                if(!stack.isEmpty() && stack.peek() == '('){
-                    stack.pop();
-                } else {
-                    stack.push(c);
+            }
+            else {
+                if (stack.isEmpty()) {
+                    return false;
                 }
+                stack.pop();
             }
         }
 
-        if (stack.isEmpty()) return true;
-        else return false;
-        
+        return stack.size() == 0;
     }
 }
