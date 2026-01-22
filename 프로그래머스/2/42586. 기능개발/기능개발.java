940c86b0a7b3a2a44d4b46d7b724af481cb935a2
miki <100952421+mikieoo@users.noreply.github.com>
2026-01-23 01:07:58 +0900

    [level 2] Title: 기능개발, Time: 0.57 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,28 +2,29 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
+
         ArrayList<Integer> list = new ArrayList<>();
         Queue<Integer> queue = new LinkedList<>();
 
         for (int i = 0; i < progresses.length; i++) {
-            int remain = (int) Math.ceil((100.0 - progresses[i]) / speeds[i]);
-            queue.offer(remain);
+            int num = (int) Math.ceil((100 - progresses[i]) / (double) speeds[i]); // 7 2 9
+            queue.offer(num); // 9 2 7
         }
 
         while (!queue.isEmpty()) {
-            int max = queue.poll(); 
+            int base = queue.poll();
             int count = 1;
 
-            while (!queue.isEmpty() && queue.peek() <= max) {
-                count++;
+            while (!queue.isEmpty() && queue.peek() <= base) {
                 queue.poll();
+                count++;
             }
 
             list.add(count);
         }
 
         int[] answer = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
+        for (int i = 0; i < answer.length; i++) {
             answer[i] = list.get(i);
         }
 
