df5f1ab7b1386ec4fcfd0b64994440b9fe17f9dd
miki <100952421+mikieoo@users.noreply.github.com>
2026-01-23 11:05:41 +0900

    [level 2] Title: 피보나치 수, Time: 1.60 ms, Memory: 81.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n) {
+        int mod = 1234567;
+        if (n <= 1) return n;
+
+        int a = 0, b = 1;
+        for (int i = 2; i <= n; i++) {
+            int temp = (a + b) % mod;
+            a = b;
+            b = temp;
+        }
+        return b;
+    }
+}
+
