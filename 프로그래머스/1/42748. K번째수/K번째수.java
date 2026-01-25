4b50092a9879453cec7b379e50583dbf776a0bc1
miki <100952421+mikieoo@users.noreply.github.com>
2026-01-25 19:53:43 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -4,18 +4,18 @@ class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
 
-        for (int cmdIdx = 0; cmdIdx < commands.length; cmdIdx++) {
-            int start = commands[cmdIdx][0]; // i 2
-            int end = commands[cmdIdx][1]; // j 5
-            int k = commands[cmdIdx][2]; // k 3
+        for (int idx = 0; idx < commands.length; idx++) {
+            int start = commands[idx][0] - 1;
+            int end = commands[idx][1] - 1;
+            int k = commands[idx][2] - 1;
 
-            // [5,2,6,3] (1,2,3,4)
             ArrayList<Integer> list = new ArrayList<>();
-            for (int i = start; i <= end; i++) { // 2 3 4 5 
-                list.add(array[i - 1]);
+            for (int x = start; x <= end; x++) {
+                list.add(array[x]);
             }
+
             Collections.sort(list);
-            answer[cmdIdx] = list.get(k - 1);
+            answer[idx] = list.get(k);
         }
 
         return answer;
